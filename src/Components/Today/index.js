aec12d7c17cadbca56f50733686d62c6acf01196
santannaizadora <santannaizadora@gmail.com>
2022-03-31 12:17:16 -0300

    feat: implements day.js

--- src/Components/Today/index.js
+++ src/Components/Today/index.js
@@ -2,22 +2,41 @@ import Footer from "../Footer";
 import Header from "../Header";
 
 import styled from 'styled-components';
+import dayjs from "dayjs";
+
+import "dayjs/locale/pt-br";
 
 const Container = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: center;
-    justify-content: center;
+    padding: 70px 20px;
     height: 100vh;
     background-color: #F2F2F2;
+
+    h1{
+        padding-top: 20px;
+        font-size: 22.976px;
+        line-height: 29px;
+        color: #126BA5;
+    }
+    h1:first-letter {
+        text-transform: capitalize;
+    }
 `
 
-export default function Today(){
-    return(
-        <Container>
-            <Header/>
+const today = dayjs().locale("pt-br").format("dddd, DD/MM");
 
-            <Footer/>
-        </Container>
+export default function Today() {
+    return (
+        <>
+            <Header />
+            <Container>
+
+                <h1>{today}</h1>
+
+            </Container>
+            <Footer />
+        </>
+
     )
 }
